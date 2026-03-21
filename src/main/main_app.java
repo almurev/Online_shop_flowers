fe45bbe533e3dcecf144074099d1e824202899b8
almurev <almurev@gmail.com>
2026-03-21 12:14:07 +0300

    написал пример кода

--- src/main/main_app.java
+++ src/main/main_app.java
@@ -1,5 +1,19 @@
 package main;
 
-public class main_app {
+import java.util.Scanner;
 
+public class main_app {
+	public static void main(String [] args) {
+		Scanner in = new Scanner(System.in);
+			
+		int a = in.nextInt();
+		byte b = in.nextByte();
+		String c = in.nextLine();
+		String s = in.next();
+		double d = in.nextDouble();
+		long e = in.nextLong();
+		short f = in.nextShort();
+			
+		System.out.println(a+""+b+""+c+""+s+""+d+""+e+""+f);
+	}
 }
